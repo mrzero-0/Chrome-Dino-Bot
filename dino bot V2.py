df5fcfa6bb8eb983075ac7b280160b25ca305a26
Devesh <deveshvmwteam@gmail.com>
2020-10-30 11:00:15 +0530

    accounted the runtime

--- dino bot V2.py
+++ dino bot V2.py
@@ -18,6 +18,10 @@ def getPixel(img, x, y):
     return pxl[x, y]
 
 
+# time variables for accounting acceleration of the Dino
+last = 0
+total_time = 0
+
 # Area of interest in the screen as rectangle can be formed using two coordinates
 # of diagonal
 x1, y1, x2, y2 = 0, 293, 1920, 465
@@ -27,17 +31,22 @@ y_cactie, x_start, x_end = 350, 435, 450
 y_bird = 275  # for the birds
 # main bot loop
 while True:
-    # Get a screen shot
-    sct_img = gui.screenshot(region=(x1, y1, x2, y2)).convert("L")
-    # sct_img.show()
+    # initial time
+    t1 = time.time()
+    # Get a screen shot of Area of interest
+    AoI = gui.screenshot(region=(x1, y1, x2, y2)).convert("L")
+    # AoI.show()
     # Get the color of the world background
-    bgColor = getPixel(sct_img, 440, 30)
+    bgColor = getPixel(AoI, 440, 30)
     # print(bgColor)
 
-    for i in reversed(range(x_start, x_end)):
+    for x in reversed(range(x_start, x_end)):
         # if i found a pixel in the search interval with a colour other than the bg colour,
         # then it is an obstacle
-        if getPixel(sct_img, i, y_cactie) != bgColor\
-                or getPixel(sct_img, i, y_bird) != bgColor:
+        if getPixel(AoI, x, y_cactie) != bgColor or getPixel(AoI, x, y_bird) != bgColor:
             keyboard.press(' ')  # jump
             break
+    # final time
+    t2 = time.time()-t1
+    total_time += t2
+    print(total_time)
