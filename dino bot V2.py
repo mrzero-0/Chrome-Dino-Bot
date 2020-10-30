e74dc50b5bc108617b77b81502cf49f1a6c29260
Devesh <deveshvmwteam@gmail.com>
2020-10-30 10:10:35 +0530

    Started working on V2

---
+++ dino bot V2.py
@@ -0,0 +1,5 @@
+'''
+This is the Version 2 of the dino bot. in this bot we tried to take screenshot
+of the area that is of interest and scan for pixels that is equal to the gray value 83,if we find
+that pixel we hit key accordingly
+'''
