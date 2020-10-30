ba1761af5479b7d65f46afc0dff6d67358833ada
Devesh <deveshvmwteam@gmail.com>
2020-10-30 09:57:13 +0530

    documented the code

--- dino bot.py
+++ dino bot.py
@@ -1,6 +1,13 @@
+'''
+This is the Version 1 of the dino bot. in this bot we tried to take screen shot
+of the screen and scan for pixels that is equal to the gray value 83,if we find
+that pixel we hit key accordingly
+'''
 import pyautogui
 from PIL import Image, ImageGrab
 import time
+
+# helper Function gor handling key press
 
 
 def hit(key):
@@ -9,13 +16,17 @@ def hit(key):
     pyautogui.keyUp(key)
     return
 
+# helper function to scan for gray pixel in the two boxes ie cactie and bird
+
 
 def isCollide(data):
+    # scanning for gray pixel in Bird box
     for i in range(240, 350):
         for j in range(410, 558):
             if data[i, j] == 83:
                 hit("down")
                 return
+    # scanning for gray pixel in cactie box
     for i in range(240, 400):
         for j in range(560, 662):
             if data[i, j] == 83:
@@ -27,11 +38,15 @@ def isCollide(data):
 if __name__ == "__main__":
     print("Welcome to Dino bot v1 your game will start in 3 sec")
     time.sleep(2)
-
+# main game loop
     while True:
+        # takes the screenshot
         image = ImageGrab.grab().convert("L")
         data = image.load()
+        # send the arrray to the scanning function
         isCollide(data)
+
+        # for tuning of the box in the screen shot
 
         # for i in range(240, 325):
         #     for j in range(560, 662):
