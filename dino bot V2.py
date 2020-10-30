c21c750e64cb0ed8bc19b1c47ecf32ac0daf24c6
Devesh <deveshvmwteam@gmail.com>
2020-10-30 10:36:05 +0530

    Added Area of interest

--- dino bot V2.py
+++ dino bot V2.py
@@ -1,5 +1,15 @@
 '''
 This is the Version 2 of the dino bot. in this bot we tried to take screenshot
-of the area that is of interest and scan for pixels that is equal to the gray value 83,if we find
-that pixel we hit key accordingly
+of the area that is of interest. then we take an arbitrary pixel of background
+and scan for that pixel in the bounding boxes of bird and cactie if the pixel is
+not equal to bgpixel then its an obstical, now we jump only when the two level of
+birds are there because the hightest bird can be passed without doing anythings
 '''
+import pyautogui as gui
+import keyboard
+import time
+import math
+
+# Area of interest in the screen as rectangle can be formed using two coordinates
+# of diagonal
+x1, y1, x2, y2 = 293, 0, 1920, 465
